c9c1201e06165df19a9b4629c16d32bccc370807
softgetix <softgetix.gallery@gmail.com>
2023-02-13 18:44:34 +0530

    prana

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'softgetix_blog_2k23' );
+define( 'DB_NAME', 'softgetix_blog_2k22_purana' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
